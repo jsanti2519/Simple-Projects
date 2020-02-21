2c82714fcae7ae62de572132aa51e05dccbb0528
jsanti2519 <53867046+jsanti2519@users.noreply.github.com>
2020-02-21 11:33:11 -0500

    Create Main.java
    Having an issue with the output of the program.

---
+++ Java/Student-List/Main.java
@@ -0,0 +1,30 @@
+import javax.swing.JOptionPane;
+/**
+@author: Jonathan Santiago
+version (1)
+Objective: Print Students' information using TextFileInput.java and a text file containing that information.
+*/
+
+class Main {
+  public static void main(String[] args) {
+    Student[] StudentList = new Student[20];
+
+    TextFileInput obj = new TextFileInput("studentnames.txt");
+
+    String line = obj.readLine();
+    int i = 0; //loop counter;
+
+    while(line != null) { //is there a neater way to do this???
+      StudentList[i].name = line;
+      line = obj.readLine();
+      StudentList[i].id = Integer.parseInt(line);
+      line = obj.readLine();
+      StudentList[i].gpa = Double.parseDouble(line);
+      line = obj.readLine();
+      i++;
+    }
+
+    for(int i = 0; i < StudentList.length; i++) //can I type StudentList[j] instead of breaking it down by variables???
+      JOptionPane.showMessageDialog(StudentList[j].name + "\n" + StudentList[j].id + "\n" + StudentList[j].gpa + "\n");
+  } //method main
+}
